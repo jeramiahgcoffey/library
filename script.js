c2aebbb7f3633f40e40f57acffec6a2c19774f21
Jeramiah Coffey <jerrygcoffey@gmail.com>
2021-10-17 01:29:36 -0500

    Main layout complete, DOM Manipulation for generating 'book-cards'

--- script.js
+++ script.js
@@ -1,16 +1,71 @@
+let myLibrary = [];
+
 function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    this.info = function () {
-        let readStatus;
-        if (!read) readStatus = "not read yet";
-        else readStatus = "completed";
-        return `${title} by ${author}, ${pages} pages, ${readStatus}`;
-    };
+    this.title = title,
+    this.author = author,
+    this.pages = pages,
+    this.read = read
+} 
+
+Book.prototype.info = function () {
+    let readStatus;
+    if (!read) readStatus = "not read yet";
+    else readStatus = "completed";
+    return `${title} by ${author}, ${pages} pages, ${readStatus}`;
 }
 
-const theHungerGames = new Book("The Hunger Games", "test", 999, true);
+function addBookToLibrary(title, author, pages, read) {
+    myLibrary.push(new Book(title, author, pages, read));
+}
 
-console.log(theHungerGames.info());
+addBookToLibrary("The Hunger Games", "test", 999, true)
+addBookToLibrary("The Hunger Games", "test", 999, true)
+addBookToLibrary("The Hunger Games", "test", 999, false)
+addBookToLibrary("The Hunger Games", "test", 999, true)
+addBookToLibrary("The Hunger Games", "test", 999, true)
+addBookToLibrary("The Hunger Games", "test", 999, true)
+addBookToLibrary("This is My Book", "Jeramiah Coffey", 1929283, true)
+// console.log(myLibrary)
+
+function displayLibrary() {
+    const library = document.querySelector("#library-container");
+
+    for (let i = 0; i < myLibrary.length; i++) {
+        const book = myLibrary[i]
+        const libraryContainer = document.querySelector("#library-container")
+
+        const newCard = document.createElement("div");
+        newCard.classList.add("card");
+        libraryContainer.appendChild(newCard);
+
+        const cardTop = document.createElement("div");
+        cardTop.classList.add("card-top");
+
+        const bookTitle = document.createElement("div");
+        bookTitle.classList.add("title");
+        bookTitle.textContent = book.title;
+        cardTop.appendChild(bookTitle);
+
+        const bookAuthor = document.createElement("div");
+        bookAuthor.classList.add("author")
+        bookAuthor.textContent = book.author;
+        cardTop.appendChild(bookAuthor);
+        newCard.appendChild(cardTop);
+
+        const bookPages = document.createElement("div");
+        bookPages.classList.add("pages");
+        bookPages.textContent = book.pages + " Pages";
+        newCard.appendChild(bookPages);
+
+        const bookReadStatus = document.createElement("div");
+        bookReadStatus.classList.add("read-status");
+        let readTextContent;
+        if (!book.read) readTextContent = "Incomplete";
+        else readTextContent = "Completed"
+        bookReadStatus.textContent = readTextContent;
+        newCard.appendChild(bookReadStatus);
+
+        libraryContainer.appendChild(newCard);
+    }
+}
+displayLibrary()
